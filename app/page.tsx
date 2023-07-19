c4da37b4afaba1db9d1debe8faf62d0dae9b63fe
Tony Spiro <tspiro@tonyspiro.com>
2023-07-19 12:53:25 -0700

    edit: site settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { createBucketClient } from "@cosmicjs/sdk"
 
 import TypesForm from "@/components/TypesForm"
@@ -33,7 +34,7 @@ export default async function IndexPage({
   }
 
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+    <section className="container grid items-center gap-4 py-6 pb-8">
       <h1 className="bold text-3xl">Algolia Extension</h1>
       {!hasCosmicContent && (
         <div>
@@ -45,30 +46,90 @@ export default async function IndexPage({
         <>
           <p>Add the following Object types to Algolia:</p>
           <div className="mb-4">
-            <div className="mb-4">
+            <div className="mb-8">
               <TypesForm types={typesTrimmed} />
+            </div>
+            <h2 className="mb-2 text-xl">Facets and more</h2>
+            <div>
+              If you would like to add / edit facets and other search settings,
+              you can do this in your{" "}
+              <a
+                href="https://dashboard.algolia.com/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-700"
+              >
+                Algolia dashboard
+              </a>
+              .
             </div>
           </div>
         </>
       )}
       {!algoliaKeysAvailable && (
         <>
-          <div>Add your Algolia keys:</div>
-          <div>1. Log in to your Algolia account</div>
-          <div>2. Go to Application / Settings / API keys</div>
-          <div>3. Copy your `Application ID`` and `Admin API key`</div>
           <div>
-            4. Go to this Extension / Settings and add the following query
-            parameters:
+            You will need to add your Algolia keys to extension settings before
+            using this extension. Follow these steps:
           </div>
+          <div>
+            1. Log in to your{" "}
+            <a
+              href="https://dashboard.algolia.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-700"
+            >
+              Algolia dashboard
+            </a>
+            .
+          </div>
+          <div>
+            <img
+              src="https://imgix.cosmicjs.com/c49a5970-266b-11ee-a19d-717742939f83-algolia-step-1.png?w=2000&auto=compression,format"
+              alt="Algolia step 1"
+              className="w-[800px]"
+            />
+          </div>
+          <div>2. Go to Application / Settings / API keys.</div>
+          <div>
+            <img
+              src="https://imgix.cosmicjs.com/c4a15e50-266b-11ee-a19d-717742939f83-algolia-step-2.png?w=2000&auto=compression,format"
+              alt="Algolia step 2"
+              className="w-[800px]"
+            />
+          </div>
+          <div>
+            3. Copy your <span className="font-bold">Application ID</span> and{" "}
+            <span className="font-bold">Admin API key</span>
+          </div>
+          <div>
+            4. In your Cosmic dashboard, go to this{" "}
+            <a
+              href={`https://app.cosmicjs.com/${bucket_slug}/extensions/marketplace`}
+              target="_parent"
+              className="text-blue-700"
+            >
+              Extension / Settings
+            </a>{" "}
+            and add the following query parameters:
+          </div>
+          <div>
+            <img
+              src="https://imgix.cosmicjs.com/94cee540-266a-11ee-a19d-717742939f83-algolia-step-3.png?w=2000&auto=compression,format"
+              alt="Algolia step 3"
+              className="w-[800px]"
+            />
+          </div>
+          <div>Here are the exact keys to add, for your copy/paste:</div>
           <div>
             <code>algolia_application_id</code>
           </div>
           <div>
             <code>algolia_admin_key</code>
           </div>
-          <div>
-            After doing this, come back to this page to add your Objects to
+          <div className="pb-10">
+            5. After doing this, come back to this page to add your Objects to
             Algolia.
           </div>
         </>
--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Cosmic Algolia Extension",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Add your Cosmic content to Algolia",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,8 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/cosmicjs",
+    github: "https://github.com/cosmicjs/cosmic-algolia-extension",
+    docs: "https://www.cosmicjs.com/docs",
   },
 }
