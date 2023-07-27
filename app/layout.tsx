77d2c0ad94115cbf3e434611ea21542e6612e84a
Karl Koch <karl@kejk.tech>
2023-07-27 09:17:24 +0100

    maybe fixes ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         </head>
         <body
           className={cn(
-            "min-h-screen bg-background font-sans antialiased",
+            "min-h-screen bg-light-background font-sans antialiased dark:bg-dark-background",
             fontSans.variable
           )}
         >
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,19 +5,21 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-light-background dark:ring-offset-dark-background",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-black dark:bg-white text-gray-900 dark:text-dark-gray-900 hover:bg-black/90 dark:hover:bg-white/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-red-500 dark:bg-dark-red-500 text-red-50 dark:text-dark-red-50 hover:bg-red-500/90 dark:hover:bg-dark-red-500/90",
         outline:
-          "border border-input hover:bg-accent hover:text-accent-foreground",
+          "border border-gray-100 dark:border-dark-gray-100 hover:bg-gray-50 dark:hover:bg-dark-gray-50 hover:text-gray-700 dark:hover:text-dark-gray-700",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "underline-offset-4 hover:underline text-primary",
+          "bg-gray-50 dark:bg-dark-gray-100 text-gray-700 dark:text-dark-gray-700 hover:bg-gray-100 dark:hover:bg-dark-gray-200",
+        ghost:
+          "hover:bg-gray-50 dark:hover:bg-dark-gray-50 hover:text-gray-700 dark:hover:text-dark-gray-700",
+        link: "underline-offset-4 hover:underline text-white dark:text-black",
       },
       size: {
         default: "h-10 py-2 px-4",
